1f1526db46b7249fc7cf1774df1c29331460c465
MrGingerNut <felixjtail03@gmail.com>
2026-07-07 15:18:42 -0600

    agregada funcionalidad para notifiacion al finalizar entrenamiento

---
+++ Mobile-UNet/Mobile-UNet_19/run_training.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while true; do
+    python3 trainModelWildfire3D4B.py --epochs 1200
+
+    if [ $? -eq 0 ]; then
+        echo "Entrenamiento completado exitosamente."
+        break
+    fi
+
+    echo "No me la hagas jochis. Reintentando en 30s..."
+    sleep 30
+done
