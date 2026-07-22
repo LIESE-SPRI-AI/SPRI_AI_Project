c93846652dad4a943fa7871f969f0c0b4472e754
MrGingerNut <felixjtail03@gmail.com>
2026-07-22 13:53:01 -0600

    fixed modelo MbUN22 final final

--- Mobile-UNet/Mobile-UNet_22/Wildfire_models.py
+++ Mobile-UNet/Mobile-UNet_22/Wildfire_models.py
@@ -3,12 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math
 
-def calc_frac(max_abs_value, signed=True, total_bits=8):
-    avail_bits = (total_bits -1) if signed else total_bits
-    int_bits = max(0, math.ceil(math.log2(max_abs_value + 1e-8))) # ?
-    frac_bits = max(0, avail_bits - int_bits)
-    return frac_bits
-
 class FakeQuantSTE(torch.autograd.Function): # strainght-through estimator
     @staticmethod
     def forward(ctx, x, scale, qmin, qmax):
@@ -19,7 +13,7 @@ class FakeQuantSTE(torch.autograd.Function): # strainght-through estimator
         ctx.save_for_backward(mask)
         return x_q * scale
 
-    @staticmethod # magia negra de claude ???
+    @staticmethod # magia negra de claude
     def backward(ctx, grad_output):
         (mask,) = ctx.saved_tensors
         grad_input = grad_output * mask.to(grad_output.dtype)
@@ -108,8 +102,7 @@ class UNet2D(nn.Module):
             nn.BatchNorm2d(32),
             nn.ReLU6(inplace=True),
             FakeQuantAct(int_bits=3, frac_bits=4, signed=False)
-        ) # 32 64 64
-
+        )
         #enconder de chava
         self.d1 = make_stage(32, 16, expand_ratio=1, n=1, stride=1) # 16 64 64
         self.d2 = make_stage(16, 24, expand_ratio=6, n=2, stride=2) # 24 32 32
@@ -118,46 +111,52 @@ class UNet2D(nn.Module):
         self.d4b = make_stage(64, 96, expand_ratio=6, n=3, stride=1) # 96 8 8
         self.d5a = make_stage(96, 160, expand_ratio=6, n=3, stride=2) # 160 4 4
         self.d5b = make_stage(160, 320, expand_ratio=6, n=1, stride=1) # 320 4 4
+        self.c1 = nn.Sequential(
+            QATConv2d(320, 1280, kernel_size=1, bias=False),
+            nn.BatchNorm2d(1280),
+            nn.ReLU6(inplace=True),
+            FakeQuantAct(int_bits=4, frac_bits=3, signed=True)
+        ) # 1280 4 4
 
         #decoder de mujer
-        self.upconv1 = QATConvTranspose2d(320, 96, kernel_size=4, stride=2, padding=1)
-        self.upconv1_quant = FakeQuantAct(int_bits=4, frac_bits=3, signed=True)
-        self.ir1 = InvertedResidual(96 + 96, 96, expand_ratio=6, stride=1)
+        self.upconv1 = QATConvTranspose2d(1280, 96, kernel_size=4, stride=2, padding=1) # 96 8 8
+        self.upconv1_quant = FakeQuantAct(int_bits=4, frac_bits=3, signed=True) # 96 8 8
+        self.ir1 = InvertedResidual(96 + 96, 96, expand_ratio=6, stride=1) # 96 8 8
 
-        self.upconv2 = QATConvTranspose2d(96, 32, kernel_size=4, stride=2, padding=1)
-        self.upconv2_quant = FakeQuantAct(int_bits=4, frac_bits=3, signed=True)
-        self.ir2 = InvertedResidual(32 + 32, 32, expand_ratio=6, stride=1)
+        self.upconv2 = QATConvTranspose2d(96, 32, kernel_size=4, stride=2, padding=1) # 32 16 16
+        self.upconv2_quant = FakeQuantAct(int_bits=4, frac_bits=3, signed=True) # 32 16 16
+        self.ir2 = InvertedResidual(32 + 32, 32, expand_ratio=6, stride=1) # 32 16 16
 
-        self.upconv3 = QATConvTranspose2d(32, 24, kernel_size=4, stride=2, padding=1)
-        self.upconv3_quant = FakeQuantAct(int_bits=4, frac_bits=3, signed=True)
-        self.ir3 = InvertedResidual(24 + 24, 24, expand_ratio=6, stride=1)
+        self.upconv3 = QATConvTranspose2d(32, 24, kernel_size=4, stride=2, padding=1) # 24 32 32
+        self.upconv3_quant = FakeQuantAct(int_bits=4, frac_bits=3, signed=True) # 24 32 32
+        self.ir3 = InvertedResidual(24 + 24, 24, expand_ratio=6, stride=1) # 24 32 32
 
-        self.upconv4 = QATConvTranspose2d(24, 16, kernel_size=4, stride=2, padding=1)
-        self.upconv4_quant = FakeQuantAct(int_bits=4, frac_bits=3, signed=True)
-        self.ir4 = InvertedResidual(16 + 16, 16, expand_ratio=6, stride=1)
+        self.upconv4 = QATConvTranspose2d(24, 16, kernel_size=4, stride=2, padding=1) # 16 64 64
+        self.upconv4_quant = FakeQuantAct(int_bits=4, frac_bits=3, signed=True) # 16 64 64
+        self.ir4 = InvertedResidual(16 + 16, 16, expand_ratio=6, stride=1) # 16 64 64
 
-        self.upconv5 = QATConvTranspose2d(16, out_channels, kernel_size=4, stride=2, padding=1)
+        self.upconv5 = QATConvTranspose2d(16, out_channels, kernel_size=4, stride=2, padding=1) # 2 128 128
 
     def forward(self, x):
         # encoder
-        x = self.stem(x) # 32 64 64
+        x = self.stem(x) # 32 64 64 
         x1 = self.d1(x) # 16 64 64
         x2 = self.d2(x1) # 24 32 32
         x3 = self.d3(x2) # 32 16 16
         x4 = self.d4b(self.d4a(x3)) # 96 8 8
-        x5 = self.d5b(self.d5a(x4)) # 320 4 4
+        x5 = self.c1(self.d5b(self.d5a(x4))) # 1280 4 4
 
         # decoder con skip connections
-        l1 = self._match_and_cat(self.upconv1_quant(self.upconv1(x5)), x4)
-        l2 = self.ir1(l1) # 16x16x96
-        l3 = self._match_and_cat(self.upconv2_quant(self.upconv2(l2)), x3)
-        l4 = self.ir2(l3) # 32x32x32
-        l5 = self._match_and_cat(self.upconv3_quant(self.upconv3(l4)), x2)
-        l6 = self.ir3(l5) # 64x64x24
+        l1 = self._match_and_cat(self.upconv1_quant(self.upconv1(x5)), x4) 
+        l2 = self.ir1(l1) # 96 8 8
+        l3 = self._match_and_cat(self.upconv2_quant(self.upconv2(l2)), x3) 
+        l4 = self.ir2(l3) # 32 16 16
+        l5 = self._match_and_cat(self.upconv3_quant(self.upconv3(l4)), x2) 
+        l6 = self.ir3(l5) # 24 32 32
         l7 = self._match_and_cat(self.upconv4_quant(self.upconv4(l6)), x1)
-        l8 = self.ir4(l7) # 128x128x16
+        l8 = self.ir4(l7) # 16 64 64
 
-        out = self.upconv5(l8) # 256x256x2 
+        out = self.upconv5(l8) # 2 128 128
 
         return out
     
@@ -167,7 +166,12 @@ class UNet2D(nn.Module):
             up = F.interpolate(up, size=skip.shape[2:], mode='bilinear', align_corners=True)
         return torch.cat([up, skip], dim=1)
     
-# Modelo principal a usar
 WildfireNet = UNet2D
 
 #funciones equis
+#def calc_frac(max_abs_value, signed=True, total_bits=8):
+    #avail_bits = (total_bits -1) if signed else total_bits
+    #int_bits = max(0, math.ceil(math.log2(max_abs_value + 1e-8))) # ?
+    #frac_bits = max(0, avail_bits - int_bits)
+#    return frac_bits
+
--- Mobile-UNet/Mobile-UNet_xx/Wildfire_models.py
+++ Mobile-UNet/Mobile-UNet_xx/Wildfire_models.py
@@ -3,12 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math
 
-def calc_frac(max_abs_value, signed=True, total_bits=8):
-    avail_bits = (total_bits -1) if signed else total_bits
-    int_bits = max(0, math.ceil(math.log2(max_abs_value + 1e-8))) # ?
-    frac_bits = max(0, avail_bits - int_bits)
-    return frac_bits
-
 class FakeQuantSTE(torch.autograd.Function): # strainght-through estimator
     @staticmethod
     def forward(ctx, x, scale, qmin, qmax):
@@ -19,7 +13,7 @@ class FakeQuantSTE(torch.autograd.Function): # strainght-through estimator
         ctx.save_for_backward(mask)
         return x_q * scale
 
-    @staticmethod # magia negra de claude ???
+    @staticmethod # magia negra de claude
     def backward(ctx, grad_output):
         (mask,) = ctx.saved_tensors
         grad_input = grad_output * mask.to(grad_output.dtype)
@@ -108,19 +102,25 @@ class UNet2D(nn.Module):
             nn.BatchNorm2d(32),
             nn.ReLU6(inplace=True),
             FakeQuantAct(int_bits=3, frac_bits=4, signed=False)
-        ) # 32 64 64
-
+        )
         #enconder de chava
-        self.d1 = make_stage(32, 16, expand_ratio=1, n=1, stride=1) # 16 64 64
-        self.d2 = make_stage(16, 24, expand_ratio=6, n=2, stride=2) # 24 32 32
-        self.d3 = make_stage(24, 32, expand_ratio=6, n=3, stride=2) # 32 16 16
-        self.d4a = make_stage(32, 64, expand_ratio=6, n=4, stride=2) # 64 8 8
-        self.d4b = make_stage(64, 96, expand_ratio=6, n=3, stride=1) # 96 8 8
-        self.d5a = make_stage(96, 160, expand_ratio=6, n=3, stride=2) # 160 4 4
-        self.d5b = make_stage(160, 320, expand_ratio=6, n=1, stride=1) # 320 4 4
+        self.d1 = make_stage(32, 16, expand_ratio=1, n=1, stride=1)
+        self.d2 = make_stage(16, 24, expand_ratio=6, n=2, stride=2)
+        self.d3 = make_stage(24, 32, expand_ratio=6, n=3, stride=2)
+        self.d4a = make_stage(32, 64, expand_ratio=6, n=4, stride=2)
+        self.d4b = make_stage(64, 96, expand_ratio=6, n=3, stride=1)
+        self.d5a = make_stage(96, 160, expand_ratio=6, n=3, stride=2)
+        self.d5b = make_stage(160, 320, expand_ratio=6, n=1, stride=1)
+
+        self.c1 = nn.Sequential(
+            QATConv2d(320, 1280, kernel_size=1, bias=False),
+            nn.BatchNorm2d(1280),
+            nn.ReLU6(inplace=True),
+            FakeQuantAct(int_bits=4, frac_bits=3, signed=True)
+        )
 
         #decoder de mujer
-        self.upconv1 = QATConvTranspose2d(320, 96, kernel_size=4, stride=2, padding=1)
+        self.upconv1 = QATConvTranspose2d(1280, 96, kernel_size=4, stride=2, padding=1)
         self.upconv1_quant = FakeQuantAct(int_bits=4, frac_bits=3, signed=True)
         self.ir1 = InvertedResidual(96 + 96, 96, expand_ratio=6, stride=1)
 
@@ -140,24 +140,24 @@ class UNet2D(nn.Module):
 
     def forward(self, x):
         # encoder
-        x = self.stem(x) # 32 64 64
+        x = self.stem(x) # 32 64 64 
         x1 = self.d1(x) # 16 64 64
         x2 = self.d2(x1) # 24 32 32
         x3 = self.d3(x2) # 32 16 16
         x4 = self.d4b(self.d4a(x3)) # 96 8 8
-        x5 = self.d5b(self.d5a(x4)) # 320 4 4
+        x5 = self.c1(self.d5b(self.d5a(x4))) # 1280 4 4
 
         # decoder con skip connections
-        l1 = self._match_and_cat(self.upconv1_quant(self.upconv1(x5)), x4)
-        l2 = self.ir1(l1) # 16x16x96
-        l3 = self._match_and_cat(self.upconv2_quant(self.upconv2(l2)), x3)
-        l4 = self.ir2(l3) # 32x32x32
-        l5 = self._match_and_cat(self.upconv3_quant(self.upconv3(l4)), x2)
-        l6 = self.ir3(l5) # 64x64x24
-        l7 = self._match_and_cat(self.upconv4_quant(self.upconv4(l6)), x1)
-        l8 = self.ir4(l7) # 128x128x16
+        l1 = self._match_and_cat(self.upconv1_quant(self.upconv1(x5)), x4) # 192 8 8
+        l2 = self.ir1(l1) # 96 8 8
+        l3 = self._match_and_cat(self.upconv2_quant(self.upconv2(l2)), x3) # 64 16 16
+        l4 = self.ir2(l3) # 32 16 16
+        l5 = self._match_and_cat(self.upconv3_quant(self.upconv3(l4)), x2) # 48 32 32
+        l6 = self.ir3(l5) # 24 32 32
+        l7 = self._match_and_cat(self.upconv4_quant(self.upconv4(l6)), x1) # 32 64 64
+        l8 = self.ir4(l7) # 16 64 64
 
-        out = self.upconv5(l8) # 256x256x2 
+        out = self.upconv5(l8) # N 128 128
 
         return out
     
@@ -171,3 +171,9 @@ class UNet2D(nn.Module):
 WildfireNet = UNet2D
 
 #funciones equis
+#def calc_frac(max_abs_value, signed=True, total_bits=8):
+    #avail_bits = (total_bits -1) if signed else total_bits
+    #int_bits = max(0, math.ceil(math.log2(max_abs_value + 1e-8))) # ?
+    #frac_bits = max(0, avail_bits - int_bits)
+#    return frac_bits
+
